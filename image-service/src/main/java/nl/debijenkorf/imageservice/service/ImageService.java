14bde0143bf813b37336e903f2c5ba89c49ca50f
Utku Ülkü <utku.ulku@hotmail.com>
2024-02-28 22:28:42 +0100

    add interface

--- image-service/src/main/java/nl/debijenkorf/imageservice/service/ImageService.java
+++ image-service/src/main/java/nl/debijenkorf/imageservice/service/ImageService.java
@@ -1,16 +1,7 @@
 package nl.debijenkorf.imageservice.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class ImageService {
-
-    public byte[] getImage(String predefinedTypeName, String dummySeoName, String reference) {
-        return new byte[0];
-    }
-
-    public void flushImage(String predefinedImageType, String reference) {
-
-    }
+public interface ImageService {
+    byte[] getImage(String predefinedTypeName, String dummySeoName, String reference);
+    void flushImage(String predefinedImageType, String reference);
 }
 
---
+++ image-service/src/main/java/nl/debijenkorf/imageservice/service/ImageServiceImpl.java
@@ -0,0 +1,14 @@
+package nl.debijenkorf.imageservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageServiceImpl implements ImageService{
+    public byte[] getImage(String predefinedTypeName, String dummySeoName, String reference) {
+        return new byte[0];
+    }
+
+    public void flushImage(String predefinedImageType, String reference) {
+
+    }
+}
