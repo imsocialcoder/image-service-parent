a919ff76856af9ba42eadc841525ab4c59fda68c
Utku Ülkü <utku.ulku@hotmail.com>
2024-03-01 04:44:38 +0100

    refactor

--- image-service/src/main/java/nl/debijenkorf/imageservice/exception/PredefinedImageTypeNotFoundException.java
+++ image-service/src/main/java/nl/debijenkorf/imageservice/exception/PredefinedImageTypeNotFoundException.java
@@ -1,10 +1,7 @@
 package nl.debijenkorf.imageservice.exception;
 
 public class PredefinedImageTypeNotFoundException extends RuntimeException {
-    private final String predefinedImageType;
-
     public PredefinedImageTypeNotFoundException(String predefinedImageType) {
         super("The requested predefined image type does not exist: " + predefinedImageType);
-        this.predefinedImageType = predefinedImageType;
     }
 }
--- image-service/src/main/java/nl/debijenkorf/imageservice/exception/SourceImageNotFoundException.java
+++ image-service/src/main/java/nl/debijenkorf/imageservice/exception/SourceImageNotFoundException.java
@@ -1,10 +1,8 @@
 package nl.debijenkorf.imageservice.exception;
 
 public class SourceImageNotFoundException extends RuntimeException {
-    private final String reference;
     public SourceImageNotFoundException(String reference) {
         super("The requested image does not exist on url, reference: " + reference);
-        this.reference = reference;
     }
 }
 
